6fd85114712581c9858c17ef27a5ea0f57b31a7c
Mohamed Amine Berroug <ma.berroug@aggrom.com>
2022-09-24 22:59:11 +0100

    added liked, follow actions

--- api/posts/controllers/posts.js
+++ api/posts/controllers/posts.js
@@ -33,14 +33,15 @@ module.exports = {
                 username: await strapi.services.userprofile.findOne({ id: posts[i].by.id }).then(result => {
                     return result.userId.username
                 }),
+                posterId: posts[i].by.id,
                 created_at: dateCreatedIn,
                 nbOfComments: posts[i].postComments.length,
                 nbOfLikes: posts[i].postLikes.length,
-                image: posts[i].images ? posts[i].images.url : null,
+                image: posts[i].images ? process.env.serverUrl + posts[i].images.url : null,
                 profileLikedIt: isRequesterLikedThePost(profileId, posts[i]),
                 profileFollowingPoster: await isRequesterFollowingPoster(profileId, posts[i].by.id),
                 posterBadge: posts[i].by.badge,
-                posterProfileImage: posts[i].by.photo ? posts[i].by.photo.url : null
+                posterProfileImage: posts[i].by.photo ? process.env.serverUrl + posts[i].by.photo.url : null
             }
             postsReturned.push(postChosen)
 
@@ -48,6 +49,90 @@ module.exports = {
         // return posts.map(entity => sanitizeEntity(entity, { model: strapi.models.posts }));
         return postsReturned
     },
+
+    async followPoster(ctx) {
+        const { posterId, followerId, action } = ctx.params;
+        let followerUserProfile = await strapi.services.userprofile.findOne({ id: followerId });
+
+        let alreadyFollowed = false
+        let indexOfFollowing = 0
+        for (let i = 0; i < followerUserProfile.following.length; i++) {
+            if (followerUserProfile.following[i].userprofile.id == followerId) {
+                alreadyFollowed = true
+                indexOfFollowing = i
+                break
+            }
+        }
+        if (action == "follow" && !alreadyFollowed) {
+
+            followerUserProfile.following.push({
+                userprofile: {
+                    id: posterId
+                },
+                when: new Date()
+            })
+            let followingNewList = followerUserProfile.following
+            await strapi.services.userprofile.update({ id: followerId }, {
+                following: followingNewList
+            });
+            return true
+
+        } else if (action == "unfollow" && alreadyFollowed) {
+            followerUserProfile.following.splice(indexOfFollowing, 1)
+            let followingNewList = followerUserProfile.following
+            await strapi.services.userprofile.update({ id: followerId }, {
+                following: followingNewList
+            });
+            return true
+        }
+
+
+        return false
+    },
+
+    async likeAPost(ctx) {
+        const { postId, likedProfileId, action } = ctx.params;
+        let post = await strapi.services.posts.findOne({ id: postId });
+        console.log("postId"+postId);
+        console.log("likedProfileId"+likedProfileId);
+        console.log("action"+action);
+        let alreadyLiked = false
+        let indexOfLiking = 0
+        for (let i = 0; i < post.postLikes.length; i++) {
+            if (post.postLikes[i].by.id == likedProfileId) {
+                alreadyLiked = true
+                indexOfLiking = i
+                break
+            }
+        }
+        console.log("action"+action);
+        if (action == "like" && !alreadyLiked) {
+
+            post.postLikes.push({
+                by: {
+                    id: likedProfileId
+                },
+                when: new Date()
+            })
+            let postLikes = post.postLikes
+            await strapi.services.posts.update({ id: postId }, {
+                postLikes: postLikes
+            });
+            return true
+
+        } else if (action == "unlike" && alreadyLiked) {
+            post.postLikes.splice(indexOfLiking, 1)
+            let postLikes = post.postLikes
+            await strapi.services.posts.update({ id: postId }, {
+                postLikes: postLikes
+            });
+            return true
+        }
+
+
+        return false
+    },
+
 
 };
 
