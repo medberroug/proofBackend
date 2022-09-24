3fd3cdc90562b44c6e36aab9cf6eb46eb7080c08
Mohamed Amine Berroug <ma.berroug@aggrom.com>
2022-09-24 21:21:24 +0100

    fixed list in community

--- api/posts/controllers/posts.js
+++ api/posts/controllers/posts.js
@@ -21,27 +21,32 @@ module.exports = {
         posts = posts.map(entity => sanitizeEntity(entity, { model: strapi.models.posts }));
         console.log(posts)
         let postsReturned = []
-        const dateCreatedIn = formatDistanceToNow(
-            posts[0].created_at,
-            { locale: eoLocale }
-        )
-        let postChosen = {
-            id: posts[0].id,
-            text: posts[0].text,
-            username: await strapi.services.userprofile.findOne({ id: posts[0].by.id }).then(result => {
-                return result.userId.username
-            }),
-            created_at: dateCreatedIn,
-            nbOfComments: posts[0].postComments.length,
-            nbOfLikes: posts[0].postLikes.length,
-            image: posts[0].images.url,
-            profileLikedIt: isRequesterLikedThePost(profileId, posts[0]),
-            profileFollowingPoster: await isRequesterFollowingPoster(profileId, posts[0].by.id),
-            posterBadge: posts[0].by.badge,
-            posterProfileImage: posts[0].by.photo.url
+
+        for (let i = 0; i < posts.length; i++) {
+            const dateCreatedIn = formatDistanceToNow(
+                posts[i].created_at,
+                { locale: eoLocale }
+            )
+            let postChosen = {
+                id: posts[i].id,
+                text: posts[i].text,
+                username: await strapi.services.userprofile.findOne({ id: posts[i].by.id }).then(result => {
+                    return result.userId.username
+                }),
+                created_at: dateCreatedIn,
+                nbOfComments: posts[i].postComments.length,
+                nbOfLikes: posts[i].postLikes.length,
+                image: posts[i].images ? posts[i].images.url : null,
+                profileLikedIt: isRequesterLikedThePost(profileId, posts[i]),
+                profileFollowingPoster: await isRequesterFollowingPoster(profileId, posts[i].by.id),
+                posterBadge: posts[i].by.badge,
+                posterProfileImage: posts[i].by.photo ? posts[i].by.photo.url : null
+            }
+            postsReturned.push(postChosen)
+
         }
         // return posts.map(entity => sanitizeEntity(entity, { model: strapi.models.posts }));
-        return postChosen
+        return postsReturned
     },
 
 };
