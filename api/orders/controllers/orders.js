eb7ca960829e712171c415f2aee3b908bf27b951
medberroug <medberroug@gmail.com>
2022-12-03 20:26:19 +0100

    DEV: added item add action in ordering

--- api/orders/controllers/orders.js
+++ api/orders/controllers/orders.js
@@ -41,19 +41,44 @@ module.exports = {
         }
         await strapi.services.orders.create(newDraft);
         return true
-    }, 
+    },
     async pedningOrdersForClient(ctx) {
         let { clientId } = ctx.params
         let pendingOrders = await strapi.services.orders.find({
             client: clientId
         });
-        for(let i=0;i<pendingOrders.length;i++){
-            let latestStatus= pendingOrders[i].status.reverse()
-            if(latestStatus[0].name=="draft"){
+        for (let i = 0; i < pendingOrders.length; i++) {
+            let latestStatus = pendingOrders[i].status.reverse()
+            if (latestStatus[0].name == "draft") {
                 return pendingOrders[i]
             }
         }
         return null
+
+    }, async addItemToOrder(ctx) {
+        let productId = ctx.request.body.productId
+        let orderId = ctx.request.body.orderId
+        let quantity = ctx.request.body.quantity
+        let product = await strapi.services.products.findOne({ id: productId });
+        let order = await strapi.services.orders.findOne({ id: orderId });
+        let taxselling = await strapi.services.tax.find();
+        taxselling = taxselling.sellingtax
+        order.items.push({
+            productId: productId,
+            quantity: quantity,
+            up: product.discount ? product.up - product.up * product.discount / 100 : product.up,
+            subTotal: product.discount ? (product.up - product.up * product.discount / 100) * quantity : product.up * quantity
+        })
+        productUp = product.discount ? product.up - product.up * product.discount / 100 : product.up
+        order.subtotal = order.subTotal + (productUp * quantity) / (1 + taxselling / 100)
+        order.tax = order.tax + productUp * (taxselling / 100)
+        order.total = order.subtotal + order.tax
+        await strapi.services.orders.update({
+            subTotal : order.subTotal,
+            total: order.total,
+            items: order.items
+        });
+        return true
     },
 
 };
