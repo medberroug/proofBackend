2742b45a45dce69303cffa5eacb764721719f460
medberroug <medberroug@gmail.com>
2022-12-04 13:34:04 +0100

    DEV: added addItemToCart and ProductAlreadyinCart API

--- api/orders/controllers/orders.js
+++ api/orders/controllers/orders.js
@@ -55,29 +55,82 @@ module.exports = {
         }
         return null
 
-    }, async addItemToOrder(ctx) {
+    },
+    async productAlreadyInCart(ctx) {
+        let { clientId, productId } = ctx.params
+        let pendingOrders = await strapi.services.orders.find({
+            client: clientId
+        });
+        for (let i = 0; i < pendingOrders.length; i++) {
+            let latestStatus = pendingOrders[i].status.reverse()
+            if (latestStatus[0].name == "draft") {
+                for (let j = 0; j < pendingOrders[i].items.length; j++) {
+                    if (pendingOrders[i].items[j].productId.id == productId) {
+                        return true
+                    }
+                }
+            }
+        }
+        return false
+
+    },
+    async addItemToOrder(ctx) {
+        let PendingOrderFound
         let productId = ctx.request.body.productId
-        let orderId = ctx.request.body.orderId
         let quantity = ctx.request.body.quantity
+        let clientId = ctx.request.body.clientId
         let product = await strapi.services.products.findOne({ id: productId });
-        let order = await strapi.services.orders.findOne({ id: orderId });
         let taxselling = await strapi.services.tax.find();
         taxselling = taxselling.sellingtax
-        order.items.push({
-            productId: productId,
-            quantity: quantity,
-            up: product.discount ? product.up - product.up * product.discount / 100 : product.up,
-            subTotal: product.discount ? (product.up - product.up * product.discount / 100) * quantity : product.up * quantity
-        })
-        productUp = product.discount ? product.up - product.up * product.discount / 100 : product.up
-        order.subtotal = order.subTotal + (productUp * quantity) / (1 + taxselling / 100)
-        order.tax = order.tax + productUp * (taxselling / 100)
-        order.total = order.subtotal + order.tax
-        await strapi.services.orders.update({
-            subTotal : order.subTotal,
-            total: order.total,
-            items: order.items
+        let pendingOrders = await strapi.services.orders.find({
+            client: clientId
         });
+        for (let i = 0; i < pendingOrders.length; i++) {
+            let latestStatus = pendingOrders[i].status.reverse()
+            if (latestStatus[0].name == "draft") {
+                PendingOrderFound = pendingOrders[i]
+                break
+            }
+        }
+        if (PendingOrderFound) {
+            let order = PendingOrderFound
+            order.items.push({
+                productId: productId,
+                quantity: quantity,
+                up: product.discount ? product.up - product.up * product.discount / 100 : product.up,
+                subTotal: product.discount ? (product.up - product.up * product.discount / 100) * quantity : product.up * quantity
+            })
+            productUp = product.discount ? product.up - product.up * product.discount / 100 : product.up
+            order.subtotal = order.subTotal + (productUp * quantity) / (1 + taxselling / 100)
+            order.tax = order.tax + productUp * (taxselling / 100)
+            order.total = order.subtotal + order.tax
+            await strapi.services.orders.update({
+                subTotal: order.subTotal,
+                total: order.total,
+                items: order.items
+            });
+            return true
+        }
+        let newDraft = {
+            client: {
+                id: clientId,
+            },
+            items: [{
+                productId: productId,
+                quantity: quantity,
+                up: product.discount ? product.up - product.up * product.discount / 100 : product.up,
+                subTotal: product.discount ? (product.up - product.up * product.discount / 100) * quantity : product.up * quantity
+            }],
+            status: [{
+                name: "draft",
+                date: new Date()
+            }],
+            subTotal: product.discount ? (product.up - product.up * product.discount / 100) * quantity / (1 + taxselling / 100) : product.up * quantity / (1 + taxselling / 100),
+            tax: product.discount ? (product.up - product.up * product.discount / 100) * quantity * (taxselling / 100) : product.up * quantity * (taxselling / 100),
+            total: product.discount ? (product.up - product.up * product.discount / 100) * quantity : product.up * quantity,
+
+        }
+        await strapi.services.orders.create(newDraft);
         return true
     },
 
